7916ad830da2b45c47e83c7eabfcd9f5098d085e
Jovhor <dr.jovhor@mail.ru>
2024-07-24 23:29:58 +0300

    добавление методов

---
+++ Nump.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListWork0._01
+{
+    class Nump
+    {
+        public int NumpID { get; set; }
+
+        public int NumpIS { get; set; }
+
+        public override string ToString()
+        {
+            return "ID: " + NumpID + "  Число: " + NumpIS;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            Nump objAsPart = obj as Nump;
+            if (objAsPart == null) return false;
+            else return Equals(objAsPart);
+        }
+        public override int GetHashCode()
+        {
+            return NumpID;
+        }
+        public bool Equals(Nump other)
+        {
+            if (other == null) return false;
+            return (this.NumpID.Equals(other.NumpID));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,34 @@ namespace ListWork0._01
     {
         static void Main(string[] args)
         {
+            List<Nump> numps = new List<Nump>();
+
+            Addy(numps);
+            Print(numps);
+
+
+
+            Console.ReadKey();
+        }
+        private static void Addy(List<Nump> list)
+        {
+            Random rand = new Random();
+
+            for (int i = 1; i <= 100; i++)
+            {
+                list.Add(new Nump() { NumpID = i, NumpIS = (int)rand.Next(1, 101) });
+            }
+        }
+
+
+        private static void Print(List<Nump> list)
+        {
+            Console.WriteLine();
+            foreach (Nump s in list)
+            {
+                Console.WriteLine(s);
+            }
         }
     }
 }
+
